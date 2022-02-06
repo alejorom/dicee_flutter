5872473f0d4766fbd150f29ccec253bad29fc9f3
alejorom <morojela-github@yahoo.com>
2022-02-05 19:16:16 -0500

    Statefull Widget

--- lib/main.dart
+++ lib/main.dart
@@ -16,28 +16,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
-        children: <Widget> [
+        children: <Widget>[
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: TextButton(onPressed: () {
-
-              },
-              child: Image.asset('images/dice1.png'),),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = 3;
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
             ),
           ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: TextButton(onPressed: () {
-
-              },
-              child: Image.asset('images/dice1.png'),),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    rightDiceNumber = 4;
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
             ),
           ),
         ],
